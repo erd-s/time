510499585ca4986e8bd5433fe2545b0da436e180
Chris Erdos <chris.erdos@gmail.com>
2021-10-05 06:20:03 -0400

    Initial Commit

---
+++ Time/TimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeApp.swift
+//  Time
+//
+//  Created by Christopher Erdos on 10/5/21.
+//
+
+import SwiftUI
+
+@main
+struct TimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
